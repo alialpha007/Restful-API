f075f7d5bbdcb5d27433774c0edb43da02492d5b
alialpha007 <94750994+alialpha007@users.noreply.github.com>
2022-06-20 15:10:52 +0500

    restful api with routes validation

---
+++ routes/api/products.js
@@ -0,0 +1,70 @@
+const express = require("express")
+let router = express.Router()
+var { Product } = require("../../models/products")
+const validateProduct = require("../../middlewares/validateProduct")
+
+//GET ALL RECORDS
+router.get("/", async (req, res) => {
+
+    let page = Number(req.query.page ? req.query.page : 1)
+    let perPage = Number(req.query.perPage ? req.query.perPage : 10)
+    let skipRecords = perPage * (page - 1)
+
+    let products = await Product.find().skip(skipRecords).limit(perPage)
+    return res.send(products)
+})
+
+//GET SINGLE RECORD
+router.get("/:id", async (req, res) => {
+
+    try {
+
+        let product = await Product.findById(req.params.id)
+
+        // WHEN ID IS CORRECT BUT PRODUCT IS NOT PRESENT IN DB
+        if (!product) return res.status(400).send("Product with given ID is not present")
+
+        // WHEN PRODUCT IS PRESENT IN DB
+        return res.send(product)
+
+    } catch (error) {
+
+        // WHEN FORMAT OF ID IS INCORRECT
+        return res.status(400).send("Invalid ID of Product...")
+    }
+})
+
+// UPDATE SINGLE RECORD (PUT)
+router.put("/:id", validateProduct, async (req, res) => {
+
+    let product = await Product.findById(req.params.id)
+    product.name = req.body.name
+    product.price = req.body.price
+    await product.save()
+    return res.send(product)
+
+})
+
+// DELETE SINGLE RECORD
+router.delete("/:id", async (req, res) => {
+
+    let product = await Product.findByIdAndDelete(req.params.id)
+    return res.send(product)
+
+})
+
+// CREATE A NEW RECORD
+router.post("/", validateProduct, async (req, res) => {
+
+
+
+    let product = new Product()
+    product.name = req.body.name
+    product.price = req.body.price
+    await product.save()
+    return res.send(product)
+
+})
+
+
+module.exports = router
